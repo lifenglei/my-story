1f72a92b5d8980a7603b9460e8002636a99a0639
lifenglei <1125911451@qq.com>
2019-11-07 19:52:29 +0800

    服务说明

--- node/index.js
+++ node/index.js
@@ -43,10 +43,12 @@ routes(app);
  */
 app.use(express.static('static'));
 
-
+/********
+ *开启服务
+ */
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('服务已经开启 骚年撸起来^^');
 });
